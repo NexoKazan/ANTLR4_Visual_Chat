8a750b7a3b22582f9f374c209837a9522e407092
Igor Kazantsev <43610577+NexoKazan@users.noreply.github.com>
2018-11-07 10:34:08 +0300

    Я не закомиттел изменения :( На гите был пустой проект.

--- ANTLR4_Chat_VisualAST/Form1.cs
+++ ANTLR4_Chat_VisualAST/Form1.cs
@@ -6,7 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 using System.Windows.Forms;
+using EntMapping.Utility;
 
 namespace ANTLR4_Chat_VisualAST
 {
@@ -15,6 +18,26 @@ namespace ANTLR4_Chat_VisualAST
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+            ICharStream inputStream = new AntlrInputStream(inputString.ToString());
+            ITokenSource speakLexer = new SpeakLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(speakLexer);
+            SpeakParser speakParser = new SpeakParser(commonTokenStream);
+            speakParser.BuildParseTree = true;
+            IParseTree tree = speakParser.root();
+            textBox1.Text = tree.ToStringTree();
+            var treeNodeDrawable = new ASTTreeNode(tree);
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+            }
+            Image image = new VisualAST(treeNodeDrawable).Draw();
+            pictureBox1.Image = image;
         }
     }
 }
---
+++ ANTLR4_Chat_VisualAST/Utility/IASTTreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace EntMapping.Utility
+{
+    public interface IASTTreeNode
+    {
+        string Text { get; }
+        string TextDef { get; }
+        int Count { get; }
+        IEnumerable<IASTTreeNode> Children { get; }
+    }
+}
